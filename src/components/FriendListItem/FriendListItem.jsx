57e86de17ece6f079e769ee47d1d8808793251b1
OleksiyMunenko <oleksiymun@gmail.com>
2023-03-24 23:12:59 +0200

    HW done

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,19 @@
+import css from './FriendListItem.module.css';
+import { FaCheckSquare, FaCircle } from 'react-icons/fa';
+
+
+
+export const FriendListItem = (friends) => {  
+    const { avatar, name, status} = friends;
+        return (
+            <li className={css.item}>
+                { (status) 
+                    ? (<span> <FaCheckSquare className={css.onlineStatusIcon} size={20} /> </span>)
+                    : (<span> <FaCircle className={css.offlineStatusIcon} size={20} /> </span>)
+                }
+
+                <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+                <p className={css.name}>{name}</p>
+            </li>
+        )
+}
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,42 @@
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+
+export const TransactionHistory = ({ transactions }) => {
+    return (
+            <table className={css.transactionHistory}>
+                <thead className={css.transactionHistoryThead}>
+                    <tr>
+                        <th>Type</th>
+                        <th>Amount</th>
+                        <th>Currency</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {transactions.map(({ id, type, amount, currency }) => {
+                        return (
+                            <tr key={id} className={css.transactionRow}>
+                                <td className={css.transactionCell}>{type}</td>
+                                <td className={css.transactionCell}>{amount}</td>
+                                <td className={css.transactionCell}>{currency}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+    )
+}
+
+
+
+TransactionHistory.propTypes = {
+    transactions: PropTypes.arrayOf(
+        PropTypes.exact({
+            id: PropTypes.string.isRequired,
+            type: PropTypes.string.isRequired,
+            amount: PropTypes.string.isRequired,
+            currency: PropTypes.string.isRequired,
+        })
+    )
+}
